64f88dcd11463b56353f3c928aba7264a491f6ea
stalko <stalko23@gmail.com>
2017-07-05 23:04:56 +0300

    - Create project

---
+++ DynamicalabsTestApplication/Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+
+namespace DynamicalabsTestApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        public class BaseModel
+        {
+            [Required]
+            [Display(Name = "File")]
+            [DataType(DataType.Upload)]
+            public IFormFile File { get; set; }
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> UploadFile(BaseModel model)
+        {
+            if (model != null && model.File != null && model.File.Length > 0)
+            {
+                switch (model.File.ContentType.ToLower())
+                {
+                    case "application/zip":
+                    case "application/x-zip":
+                    case "application/x-zip-compressed":
+                        break;
+                    default:
+                        throw new Exception("Invalid file type");
+                }
+
+                var filePath = Path.GetTempFileName();
+                try
+                {
+                    //Upload zip file to temporary folder
+                    using (var stream = new FileStream(filePath, FileMode.Create))
+                        await model.File.CopyToAsync(stream);
+
+
+                    var proc = new Models.Processing();
+                    await proc.MethodA(filePath);
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine(ex.Message);
+                    throw ex;
+                }
+                finally
+                {
+                    var fi = new FileInfo(filePath);
+                    if (fi.Exists) fi.Delete();
+                }
+            }
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ DynamicalabsTestApplication/Models/Processing.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DynamicalabsTestApplication.Models
+{
+    public class Processing
+    {
+        public async Task MethodA(string pathToZIP)
+        {
+            var tasks = new List<Task>();
+            var tmpFilesPath = new List<string>();
+
+            try
+            {
+                var zipName = Path.GetFileNameWithoutExtension(pathToZIP);
+                using (var zip = ZipFile.Open(pathToZIP, ZipArchiveMode.Read))
+                {
+                    foreach (var item in zip.Entries)
+                    {
+                        if (Path.GetExtension(item.FullName) != ".csv")
+                            continue;
+
+                        //Extract csv file to temporary folder
+                        var tmpFilePath = Path.GetTempFileName();
+                        item.ExtractToFile(tmpFilePath, true);
+                        tasks.Add(MethodB(tmpFilePath, item.Name));
+                        tmpFilesPath.Add(tmpFilePath);
+                    }
+                    //Wait all task
+                    await Task.WhenAll(tasks);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                throw ex;
+            }
+            finally
+            {
+                //Remove temporary files
+                foreach (var file in tmpFilesPath)
+                {
+                    var fi = new FileInfo(file);
+                    if (fi.Exists) fi.Delete();
+                }
+            }
+            
+        }
+
+        private async Task MethodB(string filePath, string fileName)
+        {
+            //Sleep 1 second
+            System.Threading.Thread.Sleep(1000);
+        }
+    }
+}
